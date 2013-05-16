bbc500f142a481e932da5d2c206961d01a0a5067
Vitaly Lavrov <lavrovvv@gmail.com>
2013-05-16 20:01:35 +0400

    Support OpenGeoTracker POST data

--- app/controllers/Tracker.scala
+++ app/controllers/Tracker.scala
@@ -11,4 +11,27 @@ object Tracker extends Controller {
     Ok("Position updated")
   }
 
+  def ogtUpdate(restUrl: String) = ogtupdate
+
+  def ogtupdate = Action(parse.urlFormEncoded) { implicit request =>
+    val points =
+      request.body.collect {
+        case (name, Seq(head, _ *)) =>
+          name.partition(_.isDigit) -> head
+      }
+      .groupBy {
+        case ((number, _), _) => number
+      }
+      .mapValues(_.map(t => t._1._2 -> t._2))
+      .map {
+        case (number, parameterMap) =>
+          Position(
+            BigDecimal(parameterMap("longitude")),
+            BigDecimal(parameterMap("latitude")),
+            new Date()
+          )
+      }
+    points.foreach(ObjectTracker.broadcaster.push)
+    Ok(s"Received ${points.size} points. Data: ${request.body}")
+  }
 }
