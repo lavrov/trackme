4b541aea509d8ba92afa558b043163fa31223423
Vitaly Lavrov <lavrovvv@gmail.com>
2013-05-17 12:10:37 +0400

    receiving points from OpenGeoTracker

--- app/controllers/Tracker.scala
+++ app/controllers/Tracker.scala
@@ -3,35 +3,45 @@ package controllers
 import play.api.mvc.{Action, Controller}
 import model.{Position, ObjectTracker}
 import java.util.Date
+import play.api.Logger
 
 object Tracker extends Controller {
 
-  def update(longitude: String, latitude: String) = Action {
+  def push(longitude: String, latitude: String) = Action {
     ObjectTracker.broadcaster.push(Position(BigDecimal(longitude), BigDecimal(latitude), new Date))
     Ok("Position updated")
   }
 
-  def ogtUpdate(restUrl: String) = ogtupdate
+  def ogtPush(restUrl: String) = ogtpush
 
-  def ogtupdate = Action(parse.urlFormEncoded) { implicit request =>
-    val points =
-      request.body.collect {
-        case (name, Seq(head, _ *)) =>
-          name.partition(_.isDigit) -> head
-      }
-      .groupBy {
-        case ((number, _), _) => number
-      }
-      .mapValues(_.map(t => t._1._2 -> t._2))
-      .map {
-        case (number, parameterMap) =>
-          Position(
-            BigDecimal(parameterMap("longitude")),
-            BigDecimal(parameterMap("latitude")),
-            new Date()
-          )
-      }
+  def ogtpush = Action(parse.urlFormEncoded) { implicit request =>
+    Logger.info(s"OGT data received: ${request.body}")
+    val points = request.body.collect {
+      case (PointParameterKey(index, name), values) if values.nonEmpty =>
+        (index -> name) -> values.head
+    }
+    .groupBy {
+      case ((index, _), _) => index
+    }
+    .mapValues(_.map {
+      case ((_, parameter), value) => parameter -> value
+    })
+    .map {
+      case (index, parameterMap) =>
+        Position(
+          BigDecimal(parameterMap("longitude")),
+          BigDecimal(parameterMap("latitude")),
+          new Date()
+        )
+    }
     points.foreach(ObjectTracker.broadcaster.push)
-    Ok(s"Received ${points.size} points. Data: ${request.body}")
+    Ok(s"Received ${points.size} points.")
+  }
+}
+
+object PointParameterKey {
+  def unapply(in: String) = in.partition(_.isDigit) match {
+    case (index, name) if !index.isEmpty => Some(index.toInt -> name)
+    case _ => None
   }
 }
